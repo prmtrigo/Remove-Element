91722a6959a81ae44c36c0a03fce8b7f9a4326d3
Paulo <trigopaulo@gmail.com>
2023-01-31 16:45:37 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Remove_Element;
+class Program
+{
+    public int ContarElemento(int[] vetor, int removedor){
+
+        int contador = 0; //o contador irá nos mostrar quantos números existem no vetor
+
+        for (int i=0; i<= vetor.Length-1; i++){
+            if (vetor[i] != removedor){
+                contador = contador + 1;
+            }
+        }
+        return contador;
+    }
+    static void Main(string[] args)
+    {
+        Program resultado = new Program();
+        int[] vetor = {2, 2, 3, 1, 2, 3};
+        int tamanho = vetor.Length;
+        int removedor; //definimos removedor como o número a ser removido
+        removedor = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(resultado.ContarElemento(vetor, removedor)); //aqui, estamos chamando a função!
+    }
+}
